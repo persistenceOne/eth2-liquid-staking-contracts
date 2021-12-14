33a28e96c135584ca97ee4568f40fbcebb17aad4
ciberexplosion <sachinmatta@outlook.com>
2021-12-14 13:45:06 +0530

    update

--- scripts/mainnet.js
+++ scripts/mainnet.js
@@ -4,7 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 
-// Commands to run:- 
+// Commands to run:-
 // npx hardhat node
 // npx hardhat run --network localhost scripts/deploy.js
 
@@ -18,9 +18,7 @@ const qourom = 2;
 const pStakeCommisisons = 200;
 const valCommissions = 300;
 
-
 async function main() {
-
   [treasury, user1, user2] = await hre.ethers.getSigners();
 
   const Core = await hre.ethers.getContractFactory("Core");
@@ -50,7 +48,7 @@ async function main() {
   );
   console.log("Oracle deployed to ", oracle.address);
 
-  const quorom = await oracle.updateQuorom(qourom);
+  await oracle.updateQuorom(qourom);
   console.log("Quorom initialized to ", qourom);
 
   let KeysManager = await ethers.getContractFactory("KeysManager");
@@ -58,7 +56,12 @@ async function main() {
   console.log("KeysManager deployed to ", keysManager.address);
 
   let Issuer = await ethers.getContractFactory("Issuer");
-  issuer = await Issuer.deploy(core.address, BigInt(32e32), 1000, depositContract.address);
+  issuer = await Issuer.deploy(
+    core.address,
+    BigInt(32e32),
+    1000,
+    "0x00000000219ab540356cBB839Cbe05303d7705Fa"
+  );
   console.log("Issuer deployed to ", issuer.address);
 
   let StakingPool = await ethers.getContractFactory("DummyStakingPool");
@@ -67,7 +70,10 @@ async function main() {
 
   await core.set(await core.VALIDATOR_POOL(), stakingPool.address);
   await core.set(await core.PSTAKE_TREASURY(), treasury.address);
-  await core.set(await core.WITHDRAWAL_CREDENTIAL(), "0x3d80b31a78c30fc628f20b2c89d7ddbf6e53cedc");
+  await core.set(
+    await core.WITHDRAWAL_CREDENTIAL(),
+    "0x3d80b31a78c30fc628f20b2c89d7ddbf6e53cedc"
+  );
   await core.set(await core.KEYS_MANAGER(), keysManager.address);
   await core.set(await core.ORACLE(), oracle.address);
   await core.set(await core.ISSUER(), issuer.address);
@@ -76,9 +82,17 @@ async function main() {
   await core.grantMinter(issuer.address);
   console.log("Minter granted to issuer and oracle");
 
+  await keysManager
+  .connect(defaultSigner)
+  .addValidator(
+    "0xb56720cc59e4fa235e5569dbbf1b90a746d5da9809fae4a10e31724aeb1962d948ae95f5aead9dbb7aa2c94972e5ce34",
+    "0x84739bf51b0995def38d6e744d063da983034903fc5a7e80c7cbcb05898057a047956b380be42bd128f0dce2ef98e08902a16d7152fc431809f2ced350e6535328b9a303348bed0dfb40d093046fafcd2dc9a68018bfd7496ec5d29d4fb9fa7d",
+    "0x3d80b31a78c30fc628f20b2c89d7ddbf6e53cedc"
+  );
+
   await this.issuer.connect(user1).stake({ value: BigInt(32e18) });
   await this.issuer.depositToEth2(
-    "0xb5832ad35f7713558987ce0317a480d1db394efd2b2c4a811db7bce7158bc53c8aec1ca17ea9753a2468bc9850a88ee7"
+    "0xb56720cc59e4fa235e5569dbbf1b90a746d5da9809fae4a10e31724aeb1962d948ae95f5aead9dbb7aa2c94972e5ce34"
   );
 }
 
