cb257cf0a985a2df2138c1cbdf271fde391afbe9
ciberexplosion <sachinmatta@outlook.com>
2021-12-16 18:56:11 +0530

    events and issue

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -57,6 +57,13 @@ async function main() {
   keysManager = await KeysManager.deploy(core.address);
   console.log("KeysManager deployed to ", keysManager.address);
 
+  await keysManager.addValidator(
+    "0xb56720cc59e4fa235e5569dbbf1b90a746d5da9809fae4a10e31724aeb1962d948ae95f5aead9dbb7aa2c94972e5ce34",
+    "0x84739bf51b0995def38d6e744d063da983034903fc5a7e80c7cbcb05898057a047956b380be42bd128f0dce2ef98e08902a16d7152fc431809f2ced350e6535328b9a303348bed0dfb40d093046fafcd2dc9a68018bfd7496ec5d29d4fb9fa7d",
+    "0x3d80b31a78c30fc628f20b2c89d7ddbf6e53cedc"
+  );
+  console.log("Validator added");
+
   let Issuer = await ethers.getContractFactory("Issuer");
   issuer = await Issuer.deploy(core.address, BigInt(32e32), 1000, depositContract.address);
   console.log("Issuer deployed to ", issuer.address);
@@ -75,6 +82,13 @@ async function main() {
   await core.grantMinter(oracle.address);
   await core.grantMinter(issuer.address);
   console.log("Minter granted to issuer and oracle");
+
+  await keysManager.addValidator(
+    "0xb56720cc59e4fa235e5569dbbf1b90a746d5da9809fae4a10e31724aeb1962d948ae95f5aead9dbb7aa2c94972e5ce34",
+    "0x84739bf51b0995def38d6e744d063da983034903fc5a7e80c7cbcb05898057a047956b380be42bd128f0dce2ef98e08902a16d7152fc431809f2ced350e6535328b9a303348bed0dfb40d093046fafcd2dc9a68018bfd7496ec5d29d4fb9fa7d",
+    "0x3d80b31a78c30fc628f20b2c89d7ddbf6e53cedc"
+  );
+  console.log("Validator added");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
