33ae63e5ffd5a73004947118fcfe7fad71f1a572
ciberexplosion <sachinmatta@outlook.com>
2022-01-13 11:28:56 +0530

    issuer

--- contracts/Issuer.sol
+++ contracts/Issuer.sol
@@ -8,63 +8,79 @@ import "./interfaces/IDepositContract.sol";
 import "./interfaces/IIssuer.sol";
 
 contract Issuer is CoreRef, IIssuer {
-
     uint256 public constant VALIDATOR_DEPOSIT = 32 ether;
 
     IDepositContract public DEPOSIT_CONTRACT;
     uint256 public pendingValidators;
     uint256 public minActivatingDeposit;
     uint256 public pendingValidatorsLimit;
-    
+
     mapping(address => mapping(uint256 => uint256)) public activations;
 
-    constructor(address core,
-                uint256 _minActivatingDeposit,
-                uint256 _pendingValidatorsLimit,
-                address demoDeposit
-                ) 
-        CoreRef(core) 
-    {
+    constructor(
+        address core,
+        uint256 _minActivatingDeposit,
+        uint256 _pendingValidatorsLimit,
+        address demoDeposit
+    ) CoreRef(core) {
         DEPOSIT_CONTRACT = IDepositContract(demoDeposit);
         minActivatingDeposit = _minActivatingDeposit;
 
         require(_pendingValidatorsLimit < 10000, "Issuer: invalid limit");
         pendingValidatorsLimit = _pendingValidatorsLimit;
-
     }
 
-    function activatingDeposit() public view returns (uint256 minActivatingDeposit){
+    function activatingDeposit()
+        public
+        view
+        returns (uint256 minActivatingDeposit)
+    {
         return minActivatingDeposit;
     }
 
-    function pendingValidatorLimit() public view returns (uint256 pendingValidatorsLimit){
+    function pendingValidatorLimit()
+        public
+        view
+        returns (uint256 pendingValidatorsLimit)
+    {
         return pendingValidatorsLimit;
     }
 
-    function setMinActivatingDeposit(uint256 _minActivatingDeposit) external onlyGovernor {
+    function setMinActivatingDeposit(uint256 _minActivatingDeposit)
+        external
+        onlyGovernor
+    {
         minActivatingDeposit = _minActivatingDeposit;
     }
 
-    function setPendingValidatorsLimit(uint256 _pendingValidatorsLimit) external onlyGovernor {
+    function setPendingValidatorsLimit(uint256 _pendingValidatorsLimit)
+        external
+        onlyGovernor
+    {
         require(_pendingValidatorsLimit < 10000, "Issuer: invalid limit");
         pendingValidatorsLimit = _pendingValidatorsLimit;
-    }    
+    }
 
-    function updatePendingValidator(uint256 newActiveValidators) external override {
-
-        require(core().oracle() == msg.sender, "Issuer: Only oracle can update");
+    function updatePendingValidator(uint256 newActiveValidators)
+        external
+        override
+    {
+        require(
+            core().oracle() == msg.sender,
+            "Issuer: Only oracle can update"
+        );
 
         pendingValidators = pendingValidators - newActiveValidators;
     }
 
     function mintStkEthForEth(uint256 amount, address user) internal {
-        uint256 stkEthToMint = (amount * 1e18)/stkEth().pricePerShare();
+        uint256 stkEthToMint = (amount * 1e18) / stkEth().pricePerShare();
         console.log("amount", amount);
         console.log("stkEth().pricePerShare()", stkEth().pricePerShare());
         stkEth().mint(user, stkEthToMint);
     }
 
-    function stake() payable public whenNotPaused {
+    function stake() public payable whenNotPaused {
         require(msg.value > 0, "Issuer: can't stake zero");
 
         if (msg.value <= minActivatingDeposit) {
@@ -73,43 +89,56 @@ contract Issuer is CoreRef, IIssuer {
         }
 
         // mint tokens if current pending validators limit is not exceed
-        uint256 _pendingValidators = pendingValidators + ((address(this).balance)/(VALIDATOR_DEPOSIT));
-        uint256 _activatedValidators = oracle().activatedValidators(); 
+        uint256 _pendingValidators = pendingValidators +
+            ((address(this).balance) / (VALIDATOR_DEPOSIT));
+        uint256 _activatedValidators = oracle().activatedValidators();
         uint256 validatorIndex = _activatedValidators + _pendingValidators;
         console.log("Pending val", _pendingValidators);
         console.log("active val", _activatedValidators);
         console.log("val index", validatorIndex);
 
-        if (validatorIndex * 1e4 <= _activatedValidators * (pendingValidatorsLimit + 1e4)) { // 10001
+        if (
+            validatorIndex * 1e4 <=
+            _activatedValidators * (pendingValidatorsLimit + 1e4)
+        ) {
+            // 10001
             mintStkEthForEth(msg.value, msg.sender);
             console.log("STKETH SUPPLY", stkEth().totalSupply());
-
         } else {
             // lock deposit amount until validator activated
             console.log("not minted", stkEth().totalSupply());
-            activations[msg.sender][validatorIndex] = activations[msg.sender][validatorIndex] + msg.value;
+            activations[msg.sender][validatorIndex] =
+                activations[msg.sender][validatorIndex] +
+                msg.value;
         }
     }
 
-    function activate(address _account, uint256 _validatorIndex) external whenNotPaused {
+    function activate(address _account, uint256 _validatorIndex)
+        external
+        whenNotPaused
+    {
         uint256 activatedValidators = oracle().activatedValidators();
-        console.log("activatedValidators",activatedValidators);
+        console.log("activatedValidators", activatedValidators);
 
         uint256 amount = activations[_account][_validatorIndex];
         require(amount > 0, "Issuer: invalid validator index");
         console.log("amount", amount);
-        require(_validatorIndex * 1e4 <= activatedValidators * (pendingValidatorsLimit + 1e4), "Issuer: validator is not active yet");
-
+        require(
+            _validatorIndex * 1e4 <=
+                activatedValidators * (pendingValidatorsLimit + 1e4),
+            "Issuer: validator is not active yet"
+        );
 
         delete activations[_account][_validatorIndex];
         mintStkEthForEth(amount, _account);
     }
 
     function depositToEth2(bytes calldata publicKey) external {
+        IKeysManager.Validator memory validator = IKeysManager(
+            core().keysManager()
+        ).validators(publicKey);
 
-        IKeysManager.Validator memory validator = IKeysManager(core().keysManager()).validators(publicKey);
-
-        IKeysManager(core().keysManager()).activateValidator(publicKey);
+        // IKeysManager(core().keysManager()).activateValidator(publicKey);
 
         pendingValidators = pendingValidators + 1;
         DEPOSIT_CONTRACT.deposit{value: VALIDATOR_DEPOSIT}(
@@ -118,6 +147,5 @@ contract Issuer is CoreRef, IIssuer {
             validator.signature,
             validator.deposit_root
         );
-    }  
-
+    }
 }
