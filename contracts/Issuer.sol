d50d458db36903d600906061ff6c60a1cdcbe0a6
Vedant Dalvi <vedantdalvi@Vedants-MacBook-Air.local>
2022-01-31 12:19:46 +0530

    event for mint of stk eth

--- contracts/Issuer.sol
+++ contracts/Issuer.sol
@@ -21,6 +21,8 @@ contract Issuer is CoreRef, IIssuer {
 
     bool public locked;
 
+    event MintStkEthForEth (uint256 amount, address user, uint256 stkEthToMint);
+
     mapping(address => mapping(uint256 => uint256)) public activations;
 
 
@@ -107,6 +109,7 @@ contract Issuer is CoreRef, IIssuer {
         uint256 stkEthToMint = (amount * 1e18) / stkEth().pricePerShare();
 
         stkEth().mint(user, stkEthToMint);
+        emit MintStkEthForEth (amount, user,stkEthToMint);
     }
 
 
